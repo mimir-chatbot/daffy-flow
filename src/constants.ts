11196f06a52bd9e4efdda9479e84487f42c4036d
Dany <alwe.dev@gmail.com>
2025-10-03 12:16:03 +0200

    add utility types

--- src/constants.ts
+++ src/constants.ts
@@ -11,3 +11,9 @@ export const DAFFY_TOOLS = {
     excel: 'ExcelGeneratorTool',
     postgres: 'PostgressTool',
 } as const
+
+export type FlowNodeType = typeof DAFFY_NODES[keyof typeof DAFFY_NODES]
+
+export type FlowToolType = keyof typeof DAFFY_TOOLS
+
+export type DaffyToolType = typeof DAFFY_TOOLS[FlowToolType]
